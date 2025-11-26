cbe3efe71be32487714ad72956ce9c0c6ff26e7b
narapeka <narapeka@hotmail.com>
2025-11-26 23:15:02 +0800

    Update __init__.py

--- plugins.v2/filenamecategory/__init__.py
+++ plugins.v2/filenamecategory/__init__.py
@@ -16,7 +16,7 @@ class FileNameCategory(_PluginBase):
     # 插件图标
     plugin_icon = "Bookstack_A.png"
     # 插件版本
-    plugin_version = "1.1"
+    plugin_version = "1.0"
     # 插件作者
     plugin_author = "narapeka"
     # 作者主页
